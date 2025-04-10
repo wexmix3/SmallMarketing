0509a00c0baa362675ea9ad2faa9ad768dd486b7
Max Wexley <you@example.com>
2025-04-09 21:49:11 -0400

    Add pages and app directories with basic files

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() { 
+  return ( 
+    <div> 
+      <h1>Welcome to the Marketing Platform</h1> 
+      <p><a href="/dashboard">Go to Dashboard</a></p> 
+    </div> 
+  ); 
+} 
